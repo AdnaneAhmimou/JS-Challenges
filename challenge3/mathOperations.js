5ea83e48760356386de4b6b57ac721f63a45a460
Adnane Ahmimou <ahmimouadnane102@gmail.com>
2024-02-05 16:21:20 +0100

    challenge3

---
+++ challenge3/mathOperations.js
@@ -0,0 +1,17 @@
+function addNumbers(num1, num2) {
+    return num1 + num2;
+}
+function subtractNumbers(num1, num2) {
+    return num1 - num2;
+}
+function multiplyNumbers(num1, num2) {
+    return num1 * num2;
+}
+function divideNumbers(num1, num2) {
+    return num1 / num2;
+}
+
+console.log(addNumbers(5, 5));
+console.log(subtractNumbers(5, 5)); 
+console.log(multiplyNumbers(5, 5)); 
+console.log(divideNumbers(5, 5)); 
